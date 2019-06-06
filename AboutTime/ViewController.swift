0a6e57b882bfda19437243be4eae413c5bccbbfc
Curtis S <curtis_scott@live.co.uk>
2019-06-06 10:34:30 +0100

    Initial Commit

---
+++ AboutTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutTime
+//
+//  Created by curtis scott on 06/06/2019.
+//  Copyright © 2019 CurtisScott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
